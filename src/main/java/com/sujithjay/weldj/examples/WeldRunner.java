b190046104e78df7604c2fc33a706d52576811e1
Sujith Jay Nair <sujith@sujithjay.com>
2020-04-10 16:12:35 +0100

    Portable Libs' Path

--- src/main/java/com/sujithjay/weldj/examples/WeldRunner.java
+++ src/main/java/com/sujithjay/weldj/examples/WeldRunner.java
@@ -1,14 +1,16 @@
 package com.sujithjay.weldj.examples;
 
+import java.io.File;
 import com.sujithjay.weldj.*;
 
 public class WeldRunner {
 
     static {
         try {
-            System.load("./libweldj.dylib");
+          File lib = new File("libs/osx/" + System.mapLibraryName("weldj"));
+          System.load(lib.getAbsolutePath());
         } catch (final UnsatisfiedLinkError e) {
-          System.err.println("The `weld` native library failed to load." + e);
+          System.err.println("The `weldj` native library failed to load." + e);
           System.exit(1);
         }
       }
